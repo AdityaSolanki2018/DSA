e6d623f951e5b4a361385b91078c18f9169da75d
Aditya <solankiaditya2018@gmail.com>
2026-08-11 23:26:48 +0530

    Day 5

--- Week 1 Arrays/Day4.py
+++ Week 1 Arrays/Day4.py
@@ -16,20 +16,31 @@ def sortColors(nums: list) -> None:
                 nums[red],nums[i] =nums[i],nums[red]
                 red+=1
 
-                
+#Problem: 3 Segregate Odd and Even numbers in an array
+def segregateEvenOdd(arr):
+    n = len(arr)
+    even = 0
+    for i in range(n):
+        if i%2 == 0:
+            arr[even], arr[i] = arr[i], arr[even]
+            even+=1
+
+arr = [0, 1, 2, 3, 4]
+segregateEvenOdd(arr)
+print(arr)
 # Removing duplicates from sorted array (in-place)
 # 2 pointer approach
 
-def removeDuplicates(nums) -> int:
-        if not nums:
-            return 0
-        slow = 0
-        for fast in range(1,len(nums)):
-            if nums[slow] != nums[fast]:
-                slow+=1
-                nums[slow] = nums[fast]
-        return slow + 1
+# def removeDuplicates(nums) -> int:
+#         if not nums:
+#             return 0
+#         slow = 0
+#         for fast in range(1,len(nums)):
+#             if nums[slow] != nums[fast]:
+#                 slow+=1
+#                 nums[slow] = nums[fast]
+#         return slow + 1
             
-nums = [0,0,1,1,1,2,2,3,3,4]
-k = removeDuplicates(nums)
-print(k, nums)
+# nums = [0,0,1,1,1,2,2,3,3,4]
+# k = removeDuplicates(nums)
+# print(k, nums)
--- Week 1 Arrays/Day5.py
+++ Week 1 Arrays/Day5.py
@@ -9,3 +9,17 @@ def moveZeroes(self, nums: List[int]) -> None:
         if nums[i] != 0:
             nums[zeroindex],nums[i] = nums[i],nums[zeroindex]
             zeroindex+=1
+
+
+# Best Time to Buy and Sell Stock
+def maxProfit(prices: list) -> int:
+    if not prices:
+        return 0
+    min_price = prices[0]
+    max_profit = 0
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        elif price - min_price > max_profit:
+            max_profit = price - min_price
+    return max_profit
