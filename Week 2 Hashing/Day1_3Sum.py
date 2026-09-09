3d27906d4ba8506e5a1f56f1e2c2f56b7ca6c267
Aditya <solankiaditya2018@gmail.com>
2026-09-09 05:47:35 +0530

    Week 2 Day 1 problem:3Sum Best approach

--- Week 2 Hashing/Day1_3Sum.py
+++ Week 2 Hashing/Day1_3Sum.py
@@ -43,3 +43,27 @@ def threeSum(nums):
     return list(solution)
 
 print(threeSum([0,0,0]))
+
+# Best Approach - 2 pointers in Sorted array
+def threeSum(nums):
+    nums.sort()
+    sol = []
+
+    for i in range(len(nums)):
+        if (i>0 and nums[i]==nums[i-1]):
+            continue
+        j, k = i+1, len(nums)-1
+
+        while (j < k):
+           sum = nums[i]+nums[j]+nums[k]
+        if sum<0:
+            j+=1
+        elif sum>0:
+            k-=1
+        else:
+            sol.append([nums[i],nums[j],nums[k]])
+            j+=1
+            k-=1
+
+            while (j<k and nums[j]==nums[j-1]):j+=1
+    return sol
